224503ebf5f1ab878f98a2f32443186d987d891b
phonlywithcare <phonlywithcare@gmail.com>
2025-12-31 15:39:05 +0530

    Update whatsapp.js

--- utils/whatsapp.js
+++ utils/whatsapp.js
@@ -1,19 +1,28 @@
 import axios from "axios";
 
 export const sendWhatsAppMessage = async (to, text) => {
-  await axios.post(
-    `https://graph.facebook.com/v19.0/${process.env.WA_PHONE_NUMBER_ID}/messages`,
-    {
-      messaging_product: "whatsapp",
-      to: to,
-      type: "text",
-      text: { body: text }
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.WA_ACCESS_TOKEN}`,
-        "Content-Type": "application/json"
+  try {
+    await axios.post(
+      `https://graph.facebook.com/v19.0/${process.env.WA_PHONE_NUMBER_ID}/messages`,
+      {
+        messaging_product: "whatsapp",
+        to: to,
+        type: "text",
+        text: { body: text }
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.WA_ACCESS_TOKEN}`,
+          "Content-Type": "application/json"
+        }
       }
-    }
-  );
+    );
+
+    console.log("WhatsApp sent to:", to);
+  } catch (error) {
+    console.error(
+      "WhatsApp error:",
+      error.response?.data || error.message
+    );
+  }
 };
